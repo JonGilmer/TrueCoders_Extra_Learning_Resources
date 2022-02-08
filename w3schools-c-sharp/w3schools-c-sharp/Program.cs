167a782ddae744742fc196254385b3ba5e4f49b9
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-08 12:05:41 -0500

    initial push to GitHub

---
+++ w3schools-c-sharp/w3schools-c-sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace w3schoolscsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+/* Pres F5 to run the program with debugging */
